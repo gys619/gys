3258e6cf23591eb13d800b092ea12f72f3d0b4b5
gys619 <59304891+gys619@users.noreply.github.com>
2021-05-14 13:21:43 +0800

    Create jddj_cookie.js

---
+++ jddj_cookie.js
@@ -0,0 +1,7 @@
+//nodejs专用,手机用boxjs填写
+//cookie只要里面的deviceid_pdj_jd=xxx-xxx-xxx;o2o_m_h5_sid=xxx-xxx-xxx关键信息
+let cookies = [
+  'deviceid_pdj_jd=077655c5a53e4ee0;o2o_m_h5_sid=84b4863c-bbc7-4702-9a2c-7762f22a11ee',
+  'deviceid_pdj_jd=407f28c1b469f48a;o2o_m_h5_sid=109e23f8-90f0-4c99-80be-ae70f97243e8'
+];
+module.exports = cookies;
