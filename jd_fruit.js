efd28ce8d1ee3fef4f0469b15dce456377651dd4
gys619 <59304891+gys619@users.noreply.github.com>
2021-08-17 23:04:34 +0800

    Update jd_fruit.js

--- jd_fruit.js
+++ jd_fruit.js
@@ -11,10 +11,10 @@
 ==========================Quantumultx=========================
 [task_local]
 #jd免费水果
-5 6-18/6 * * * jd_fruit.js, tag=东东农场, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdnc.png, enabled=true
+5 0,6-18/6 * * * jd_fruit.js, tag=东东农场, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdnc.png, enabled=true
 =========================Loon=============================
 [Script]
-cron "5 6-18/6 * * *" script-path=jd_fruit.js,tag=东东农场
+cron "5 0,6-18/6 * * *" script-path=jd_fruit.js,tag=东东农场
 
 =========================Surge============================
 东东农场 = type=cron,cronexp="5 6-18/6 * * *",wake-system=1,timeout=3600,script-path=jd_fruit.js
